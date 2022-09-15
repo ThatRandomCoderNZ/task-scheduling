f9c91eafc0edfe359ea79ffb99ecd3ccadc67372
ethan.mitchell <ethan.mitchell@figured.com>
2022-09-15 12:52:53 +1200

    linearised constraint / switching numVar to boolVar

--- src/concepts/MixedIntegerProgram.java
+++ src/concepts/MixedIntegerProgram.java
@@ -4,6 +4,7 @@ import ilog.concert.*;
 import ilog.cplex.IloCplex;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MixedIntegerProgram implements LinearProgramSolution{
@@ -17,19 +18,26 @@ public class MixedIntegerProgram implements LinearProgramSolution{
 
         //Task 1 - cost 1, Task 2 - cost 2, Task 3 - cost 3
         int[][] allFeasibleSchedules = {
-                {1},{1,2},{1,2,3},{2,3},{3},{3,2},{2},{3,1}
-        };
-        int[] costOfSchedule = {1,3,7,6,4,6,2,5};
+                {1},{1,2},{1,2,3}//,{2,3},{3},{3,2},{2},{3,1}
+        };                    //1 2 3 4 5 6 7 8
+        int[] costOfSchedule = {1,3,7};//,6,4,6,2,5};
         int[][] jobIsCovered = {
-                {1, 1, 1, 0, 0, 0, 0, 1},
-                {0, 1, 1, 1, 0, 1, 1, 0},
-                {0, 0, 1, 1, 1, 1, 0, 1}
+                //  2  3  4  5  6  7  8
+                {1, 1, 1, },//0, 0, 0, 0, 1},
+                {0, 1, 1, },//1, 0, 1, 1, 0},
+                {0, 0, 1, },//1, 1, 1, 0, 1}
         };
+
+
 
         IloNumVar[][] scheduleIsUsed = new IloNumVar[numOfMachines][];
         for(int i = 0; i < numOfMachines; i++){
             scheduleIsUsed[i] = cplex.boolVarArray(costOfSchedule.length);
         }
+
+        IloNumVar[] z = new IloNumVar[1];
+        z[0] = cplex.numVar(0, 20);
+
 
         IloNumExpr[] objFunc = new IloNumExpr[numOfMachines];
         for(int index = 0; index < numOfMachines; index++){
@@ -37,16 +45,39 @@ public class MixedIntegerProgram implements LinearProgramSolution{
             objFunc[index] = cplex.sum(expr, cplex.scalProd(costOfSchedule, scheduleIsUsed[index]));
         }
 
-        cplex.addMinimize(cplex.max(objFunc));
+        cplex.addMinimize(cplex.sum(z));
 
+
+        IloRange[] constraints = new IloRange[numOfJobs];
         for(int i = 0; i < numOfJobs; i++){
             IloNumExpr jobExecutedOnceConstraint = cplex.numExpr();
             for(int j = 0; j < numOfMachines; j++) {
                 jobExecutedOnceConstraint = cplex.sum(jobExecutedOnceConstraint, cplex.scalProd(scheduleIsUsed[j], jobIsCovered[i]));
             }
-            cplex.addEq(jobExecutedOnceConstraint, 1);
+            IloRange range = cplex.addEq(jobExecutedOnceConstraint, 1);
+            constraints[i] = range;
         }
 
+//        for(int i = 0; i < allFeasibleSchedules.length; i++){
+//            IloNumExpr scheduleExecutedOnce = cplex.numExpr();
+//            for(int j = 0; j < numOfMachines; j++) {
+//                scheduleExecutedOnce = cplex.sum(scheduleExecutedOnce, cplex.min(cplex.prod(scheduleIsUsed[j][i], 100), 1));
+//            }
+//            cplex.addLe(scheduleExecutedOnce, 1);
+//        }
+
+        IloRange[] zConstraints = new IloRange[numOfMachines * allFeasibleSchedules.length];
+        int index = 0;
+        for(int m = 0; m < numOfMachines; m++){
+            for(int s = 0; s < allFeasibleSchedules.length; s++){
+                IloNumExpr test = cplex.prod(scheduleIsUsed[m][s], costOfSchedule[s]);
+                IloRange range = (IloRange) cplex.addLe(test, z[0]);
+                zConstraints[index] = range;
+                index++;
+            }
+        }
+
+        IloRange[] machineConstraints = new IloRange[numOfMachines];
         IloNumExpr totalMachines = cplex.numExpr();
         for(int i = 0; i < numOfMachines; i++) {
             totalMachines = cplex.sum(totalMachines, cplex.sum(scheduleIsUsed[i]));
@@ -71,6 +102,17 @@ public class MixedIntegerProgram implements LinearProgramSolution{
                 }
                 System.out.println();
             }
+
+            System.out.println("Dual Variables");
+            //System.out.println(Arrays.toString(zConstraints));
+            //double[] range = cplex.getDuals(zConstraints);
+            //System.out.println(Arrays.toString(range));
+
+            System.out.println("Reduced Costs");
+            for(int i = 0; i < numOfMachines; i++) {
+                System.out.println(Arrays.toString(cplex.getReducedCosts(scheduleIsUsed[i])));
+            }
+
 //
 
         }
