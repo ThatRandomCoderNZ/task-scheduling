f68458e17a917a69430a632120b3bfc6ba79f749
ethan.mitchell <ethan.mitchell@figured.com>
2022-10-06 15:23:52 +1300

    can now read from file and solve

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,47 @@
 import concepts.*;
 import ilog.concert.IloException;
-import ilog.concert.IloNumVar;
-import ilog.cplex.IloCplex;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) throws IloException {
+    public static void main(String[] args) throws IloException, FileNotFoundException {
         TaskScheduler taskScheduler = new HeuristicSolution();
 
         ProblemInputHandler inputHandler = new ProblemInputHandler();
+        String fileName = "LL1";
+        List<List<String>> taskData = inputHandler.getData(fileName, true);
+        List<List<String>> comData = inputHandler.getData(fileName, false);
 
+        List<Integer> taskCosts = new ArrayList<>();
+        for(List<String> record: taskData){
+            for(String data: record){
+                int cost = Integer.parseInt(data.trim().replaceAll("\\s+", " ").split(" ")[1]);
+                taskCosts.add(cost);
+            }
+        }
+        taskCosts.remove(0);
+        List<Integer> comCosts = new ArrayList<>();
+        for(List<String> record: comData){
+            for(String recordData: record){
+                String[] data = recordData.trim().replaceAll("\\s+", " ").split(" ");
+                comCosts.add(Integer.parseInt(data[2]));
+            }
+        }
 
-//        TaskList taskList = new TaskList(10);
+        List<Task> taskDataList = new ArrayList<>();
+        for(int i = 0; i < taskCosts.size(); i++){
+            Task task = new Task(i, taskCosts.get(i), comCosts.get(i));
+            taskDataList.add(task);
+        }
+
+        List<Integer> initialSchedule = taskDataList.stream().map(task -> task.id).collect(Collectors.toList());
+        int[][] initialSchedules = {initialSchedule.stream().mapToInt((Integer val) -> val).toArray()};
+
+        TaskList taskList = new TaskList(taskDataList);
 //        int[][] initialSchedules = {
 //                {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
 //                {0, 1, 2, 3},
@@ -19,7 +49,7 @@ public class Main {
 //                {4, 5, 6, 7}
 //        };
 //
-//        taskScheduler.solve(taskList, initialSchedules);
+       taskScheduler.solve(taskList, initialSchedules);
     }
 }
 
--- src/concepts/ProblemInputHandler.java
+++ src/concepts/ProblemInputHandler.java
@@ -1,6 +1,46 @@
 package concepts;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ProblemInputHandler {
 
+    public static final String PROBLEM_FILE_DIR = "problem-input";
+    public static final String FILE_DIR_SEPARATOR = "/";
+    public static final String FILE_NAME_SEPARATOR = "_";
+    public static final String CSV_DELIMITER = ",";
+    public static final String TASK_DATA = "TD";
+    public static final String COM_DATA = "CDD";
+    public static final String CSV_EXT = ".csv";
+
+    public List<List<String>> getData(String fileName, boolean isTaskData) throws FileNotFoundException {
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(
+                createFilePath(fileName, isTaskData)))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(CSV_DELIMITER);
+                records.add(Arrays.asList(values));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return records;
+    }
+
+    public String createFilePath(String fileName, boolean isTaskData){
+        String dataType = isTaskData ? TASK_DATA : COM_DATA;
+        return "" + PROBLEM_FILE_DIR + FILE_DIR_SEPARATOR + dataType + FILE_NAME_SEPARATOR + fileName + CSV_EXT;
+    }
+
+    public String createFilePath(String fileName){
+        return createFilePath(fileName, true);
+    }
+
+    public void getComData(){
+
+    }
 
 }
